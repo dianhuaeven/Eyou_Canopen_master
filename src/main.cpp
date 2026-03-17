538d0106601ddf59b2ff4ffdcdb7b97a262f1d87
dianhuaeven <dianhuaeven@gmail.com>
2026-03-17 18:43:04 +0800

    app: add main loop and launch skeleton

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <atomic>
+#include <chrono>
+#include <csignal>
+#include <thread>
+
+#include "canopen_hw/canopen_master.hpp"
+#include "canopen_hw/canopen_robot_hw.hpp"
+#include "canopen_hw/shared_state.hpp"
+
+namespace {
+
+std::atomic<bool> g_run{true};
+
+void HandleSignal(int) {
+  g_run.store(false);
+}
+
+}  // namespace
+
+int main() {
+  std::signal(SIGINT, HandleSignal);
+  std::signal(SIGTERM, HandleSignal);
+
+  canopen_hw::SharedState shared_state;
+
+  canopen_hw::CanopenMasterConfig master_cfg;
+  master_cfg.can_interface = "can0";
+  master_cfg.master_node_id = 127;
+  master_cfg.axis_count = 6;
+  master_cfg.master_dcf_path = "config/master.dcf";
+
+  canopen_hw::CanopenMaster master(master_cfg, &shared_state);
+  canopen_hw::CanopenRobotHw robot_hw(&shared_state);
+
+  if (!master.Start()) {
+    return 1;
+  }
+
+  // 应用主循环骨架: 100Hz 执行 read/write。
+  // 后续接入 ROS controller_manager 后，这里将替换为 ros::Rate + cm.update()。
+  const auto period = std::chrono::milliseconds(10);
+  while (g_run.load()) {
+    robot_hw.ReadFromSharedState();
+    robot_hw.WriteToSharedState();
+    std::this_thread::sleep_for(period);
+  }
+
+  master.Stop();
+  return 0;
+}
